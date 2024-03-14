255bf276a948268097707d2ba6a149364ba4b362
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-03-14 14:29:46 -0600

    add the logo on the text of the Getting started

--- app/_components/navigation-menu.tsx
+++ app/_components/navigation-menu.tsx
@@ -14,6 +14,7 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import { Button } from "@/components/ui/button"
+import Computer from "@/components/icons/computer"
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -58,7 +59,7 @@ export function NavigationMenuDemo() {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-        <NavigationMenuTrigger><Button variant="destructive">Getting started</Button></NavigationMenuTrigger>
+        <NavigationMenuTrigger><Button>Getting started</Button></NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
@@ -69,12 +70,12 @@ export function NavigationMenuDemo() {
                       className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                       href="/"
                     >
+                      <Computer />
                       <div className="mb-2 mt-4 text-lg font-medium">
-                        shadcn/ui
+                        Elevate you online presence with your own domain and customized Page.
                       </div>
                       <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components built with Radix UI and
-                        Tailwind CSS.
+                        Are you ready to stand out in the digital world? Take your online presence to the next level with your very own domain and a tailor-made web page!
                       </p>
                     </a>
                     
